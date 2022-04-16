12c3eea4af7dca336ebda7f31945c91820f6a73c
kozhemyakin <pctdoc@gmail.com>
2022-04-16 12:29:25 +0400

    solve task with boxes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Text from "./Text";
+import Box from "./Box";
 
 function App() {
   return (
-    <div className="App">
-      <Text text="Regular text" />
-      <Text text="Huge header" fontSize='32' />
-      <Text text="Danger notification" textColor='red' />
-      <Text text="Underlined text" fontSize='22' underline />
+    // <div className="App">
+    //   <Text text="Regular text" />
+    //   <Text text="Huge header" fontSize='32' />
+    //   <Text text="Danger notification" textColor='red' />
+    //   <Text text="Underlined text" fontSize='22' underline />
+    // </div>
+
+    <div className='boxes'>
+      <Box cssStyle='big' label='BIG' />
+      <Box cssStyle='medium' label='MEDIUM' />
+      <Box cssStyle='small' label='SMALL' />
     </div>
   );
 }
